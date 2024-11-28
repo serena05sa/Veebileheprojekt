b612c1bdece1f160446de078296f32437fe60f3e
Miia Isabel Anipai <miiaanipai@Miias-Air.lan>
2024-11-28 20:45:29 +0200

    js kommentaarid (miia)

--- trykkimise_efekt.js
+++ trykkimise_efekt.js
@@ -1,4 +1,5 @@
 // Küsisin abi ChatGPT käest
+// Fraasid, mis kirjutamisefektiga ekraanile kuvatakse
 document.addEventListener("DOMContentLoaded", function () {
     const phrases = [
         "rahvaarvult Eesti teine linn",
@@ -11,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "#729B79", 
     ];
 
+// Parameetrid nagu kirjutamiskiirus, mis fraasi või sõna juures oleme, kas oleme kustutamise faasis, kuhu tekst kuvatakse
     let currentPhraseIndex = 0;
     let currentLetterIndex = 0;
     let isDeleting = false;
@@ -19,23 +21,28 @@ document.addEventListener("DOMContentLoaded", function () {
     const delay = 2000;
     const element = document.querySelector(".animeeri span");
 
+    // Kirjutamisefekti osa
     function type() {
         element.style.color = colors;
+    // Kui tegemist kustutamise funktsiooniga, hakkame kustutama ühe tähe haaval
         if (isDeleting) {
             if (currentLetterIndex > 0) {
                 currentLetterIndex--;
                 element.textContent = phrases[currentPhraseIndex].substring(0, currentLetterIndex);
                 setTimeout(type, deleteSpeed);
+                // Kui kõik tähed kustutatud, liigume kirjutamisreiimi
             } else {
                 isDeleting = false;
                 currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
                 setTimeout(type, speed);
             }
+            // Kontroll, kas praegune fraas on kirjutatud
         } else {
             if (currentLetterIndex < phrases[currentPhraseIndex].length) {
                 element.textContent = phrases[currentPhraseIndex].substring(0, currentLetterIndex + 1);
                 currentLetterIndex++;
                 setTimeout(type, speed);
+                // Kui fraas on kirjutatud, hakkame jälle kustutama
             } else {
                 isDeleting = true;
                 setTimeout(type, delay);
